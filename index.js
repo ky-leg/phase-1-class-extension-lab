c466ede81a1d3634dd7c96ba5d15223680178bde
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-12-22 13:01:35 -0500

    commit tree eb6932b269e4d82ef034ab1d4be0d10b29335b95

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+class Polygon {
+    constructor(array){
+        this.sides = array.length;
+        this.addedUpSides = array.reduce((x, y) => x + y)
+        this.array = array
+    }
+    get countSides(){
+        return this.sides
+    }
+    
+    get perimeter(){
+        return this.addedUpSides
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        let array = this.array
+        let hasThreeSides = this.countSides === 3
+        let isProperRatio = (
+            array[0] + array[1] > array[2] &&
+            array[1] + array[2] > array[0] &&
+            array[2] + array[0] > array[1]
+        )
+        return (hasThreeSides && isProperRatio)
+    }
+}
+
+class Square extends Polygon{
+    get area(){
+        return this.array[0]**2
+    }
+    get isValid(){
+        let array= this.array
+        let hasFourSides = this.countSides === 4 
+        let isProperRatio = (
+                array[0] === array[1] &&
+                array[0] ===array[2] &&
+                array[0] ===array[3]
+            )
+        return (hasFourSides && isProperRatio)
+    }
+}
+
+const triangle = new Triangle([1,2,2])
+console.log(triangle.perimeter)
